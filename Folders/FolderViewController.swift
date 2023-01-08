135af49803002f91ce1b3447f22c353f7645b5dd
Pablo Romero <romero7116@gmail.com>
2023-01-08 12:12:47 +0100

    Set initial empty project

---
+++ Folders/FolderViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FolderViewController.swift
+//  Folders
+//
+//  Created by Pablo Ezequiel Romero Giovannoni on 07/01/2023.
+//
+
+import UIKit
+
+class FolderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        navigationItem.title = "Folder"
+    }
+
+}
